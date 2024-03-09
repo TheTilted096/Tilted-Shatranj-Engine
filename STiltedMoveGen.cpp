9736e8776649e03d4650b013cf9249bf46cdc145
TheTilted096 <katchupfrixxions2@gmail.com>
2024-03-08 21:18:23 -0800

    Test Header 2

--- STiltedMoveGen.cpp
+++ STiltedMoveGen.cpp
@@ -1,13 +1,9 @@
 /*
-
 This is take 2 of the Tilted Shatranj Engine,
 since git decided it was a great idea to kill everybody. 
 
 Anyways, this is a library to do movegen. 
 
-Nathaniel Potter, 3-8-2024. 
-
-TheTilted096
-
+TheTilted096, 3-8-2024. 
 */
 
