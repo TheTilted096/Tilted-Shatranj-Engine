da3ac07f4be59a7635b74f685a25b3d391618bec
TheTilted096 <katchupfrixxions2@gmail.com>
2024-03-08 21:37:57 -0800

    Bitboard Printing

---
+++ STiltedDriver.cpp
@@ -0,0 +1,16 @@
+/*
+
+Driver Code for the Tilted Shatranj Engine
+
+TheTilted096, 2024. 
+
+*/
+
+#include "STiltedMoveGen.cpp"
+
+int main(){
+
+    printAsBitboard(RANK0);
+
+    return 0;
+}
--- STiltedMoveGen.cpp
+++ STiltedMoveGen.cpp
@@ -7,3 +7,30 @@ Anyways, this is a library to do movegen.
 TheTilted096, 3-8-2024. 
 */
 
+#include <iostream>
+#include <bit>
+#include <cstdint>
+
+#define RANK0 255ULL //8th rank
+#define FILE0 72340172838076673ULL //A File
+
+/*
+The plan:
+
+Each side is still represented as 7 bitboards. 
+
+Order of Pieces; King, Rook, Knight, Ferz, Alfil, Pawn, ALL
+*/
+
+void printAsBitboard(uint64_t board){
+    for (int i = 0; i < 8; i++){
+        std::cout << (board & 255ULL) << '\n';
+        board >>= 8;
+    }
+}
+
+
+
+uint64_t emptyBoardMoves(int square, int pieceType){
+    return 0;
+}
